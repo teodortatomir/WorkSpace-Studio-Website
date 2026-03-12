1cace31ab48d437c86e22948bf6b6e5113ff6dab
Teodor Tatomir <teodortatomir@yahoo.com>
2026-03-12 18:48:49 +0100

    Made modifications for hero -> Added Video

--- WorkSpace-studio-demo/projects-scene/ProjectsScene.jsx
+++ WorkSpace-studio-demo/projects-scene/ProjectsScene.jsx
@@ -1,15 +1,26 @@
 const { motion } = window.Motion;
 
 const projects = [
-    { id: 1, title: "UiPath", cat: "Global Headquarters", img: "../images/uipath-header.jpg", link: "../projects/UiPath/uipath.html" },
-    { id: 2, title: "Adobe", cat: "Romania Hub", img: "../images/adobe-header.jpg", link: "#" },
-    { id: 3, title: "Tiriac Collection", cat: "Exclusive Automotive Gallery", img: "../projects-pictures/tiriac-collection.jpg", link: "#" },
-    { id: 4, title: "Honeywell", cat: "Research & Ops", img: "../images/honeywell-header.jpg", link: "#" },
-    { id: 5, title: "OMV Petrom", cat: "Corporate Campus", img: "../images/omvpetrom-header.jpg", link: "#" },
-    { id: 6, title: "Unilever", cat: "Sustainability Office", img: "../images/unilever-header.jpg", link: "#" },
-    { id: 7, title: "BBraun", cat: "Innovation Center", img: "../projects-pictures/bbraun.jpg", link: "#" },
-    { id: 8, title: "DataCore", cat: "Tech Hub", img: "../projects-pictures/datacore.jpg", link: "#" },
-    { id: 9, title: "InteRo Property Development", cat: "High-End Residential & Office", img: "../projects-pictures/intero.jpg", link: "#" }
+    { id: 1, title: "OMV Petrom", cat: "Corporate Campus", img: "../images/omvpetrom-header.jpg", link: "#" },
+    { id: 2, title: "Tiriac Collection", cat: "Exclusive Automotive Gallery", img: "../projects-pictures/tiriac-collection.jpg", link: "#" },
+    { id: 3, title: "L'Oreal Romania", cat: "Beauty", img: "../projects-pictures/loreal-romania.jpg", link: "#"},
+    { id: 4, title: "Tchibo", cat: "Coffe", img: "../projects-pictures/tchibo.jpg", link: "#"},
+    { id: 5, title: "BBraun", cat: "Innovation Center", img: "../projects-pictures/bbraun.jpg", link: "#" },
+    { id: 6, title: "DataCore", cat: "Tech Hub", img: "../projects-pictures/datacore.jpg", link: "#" },
+    { id: 7, title: "InteRo Property Development", cat: "High-End Residential & Office", img: "../projects-pictures/intero.jpg", link: "#" },
+    { id: 8, title: "Legrand", cat: "Legrand", img: "../projects-pictures/legrand.jpg", link: "#"},
+    { id: 9, title: "Loreal Academy", cat: "Loreal Academy", img: "../projects-pictures/loreal.jpg", link: "#"},
+    { id: 10, title: "Honeywell", cat: "Research & Ops", img: "../images/honeywell-header.jpg", link: "#" },
+    { id: 11, title: "Urban Bistro, ISHO", cat: "ISHO", img: "../projects-pictures/urbanbistro-isho.jpeg", link: "#"},
+    { id: 12, title: "AJ Brand", cat: "AJ Brand", img: "../projects-pictures/aj.jpg", link: "#"},
+    { id: 13, title: "Mindspace", cat: "Mindspace", img: "../projects-pictures/mindspaces.jpg", link: "#"},
+    { id: 14, title: "Grupo Bimbo", cat: "Grupo Bimbo", img: "../projects-pictures/bimbo.jpg", link: "#"},
+    { id: 15, title: "Diversinst", cat: "Diversinst", img: "../projects-pictures/diversinst.jpg", link: "#"},
+    { id: 16, title: "TMF Group", cat: "TMF Group", img: "../projects-pictures/tmf-group.jpg", link: "#"},
+    { id: 17, title: "Profi", cat: "Profi", img: "../projects-pictures/profi.jpg", link: "#"},
+    { id: 18, title: "UiPath", cat: "Global Headquarters", img: "../images/uipath-header.jpg", link: "../projects/UiPath/uipath.html" },
+    { id: 19, title: "Adobe", cat: "Romania Hub", img: "../images/adobe-header.jpg", link: "#" },
+    { id: 20, title: "Unilever", cat: "Sustainability Office", img: "../images/unilever-header.jpg", link: "#" }
 ];
 
 function ProjectsScene() {
@@ -43,7 +54,7 @@ function ProjectsScene() {
                             whileInView={{ opacity: 1, y: 0 }}
                             transition={{ 
                                 duration: 1, 
-                                delay: (index % 3) * 0.1, // Delay optimizat pentru 3 coloane
+                                delay: (index % 3) * 0.1,
                                 ease: [0.16, 1, 0.3, 1] 
                             }}
                             viewport={{ once: true, margin: "-50px" }}
